3bd6c0dec16174993eb190e692c58372e84e3bfa
Vivan <vivanpanda13@gmail.com>
2023-07-18 21:16:40 +0400

    Loop through array and console log

--- script.js
+++ script.js
@@ -10,15 +10,30 @@ submitBookButton.addEventListener('click', () => {
     modal.close();
 })
 
-let library = [];
+let library = [
+    new Book('Book 1', 'Author 1', '100 pages'),
+    new Book('Book 2', 'Author 2', '100 pages'),
+    new Book('Book 3', 'Author 3', '100 pages')
+];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+}
+
+Book.prototype.readStatus = function() {
+
+}
+
+Book.prototype.removeBook = function() {
+
 }
   
 function addBookToLibrary() {
 
+}
+
+for (let i = 0; i < library.length; i++) {
+    console.log(library[i].author)
 }
