870c9e81b1af3b7378b633998162aef2ee54c969
Vivan <vivanpanda13@gmail.com>
2023-07-18 11:42:00 +0400

    Create modal form basic

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const newBookButton = document.querySelector('.new-book-button')
+const submitBookButton = document.querySelector('.submit-book-button')
+const modal = document.querySelector('.modal');
 
+newBookButton.addEventListener('click', () => {
+    modal.showModal();
+})
+
+submitBookButton.addEventListener('click', () => {
+    modal.close();
+})
 
 let library = [];
 
