4912e80bc7d0d91077555d4edef3e961aec649e0
Vivan <vivanpanda13@gmail.com>
2023-07-25 10:03:52 +0400

    Refactor code to use classes

--- script.js
+++ script.js
@@ -13,11 +13,13 @@ submitBookButton.addEventListener('click', () => {
   
 let library = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
   
 function addBookToLibrary() {
